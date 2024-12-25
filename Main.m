e00fb2a328ac74b19027ee293ff0be467df2b9ed
Narkhede <anarkhede@SEASHELL.cntrlsrvs.w2k.vt.edu>
2024-12-25 13:39:12 -0500

    Modified code. The solver uses volume fraction to track each fluid material explicitly in the domain.
    Testing. Currently does not work.

--- Main.m
+++ Main.m
@@ -4,28 +4,27 @@ clc;
 
 % This code simulates the PG-PG example case from Arthur Rallu's thesis Section 4.8
 % Here, we model both materials with Stiffened gas equation of state with pstiff = 0.
+% We use the five equation model, which tracks the fractional density of each fluid 
+% material. The volume fraction of fluid material is advected in space and time to
+% calculate relevant pressure and internal energy in the control volume.
+% NOTE: This code is intended to work only for two fluid materials. The code will
+% be expanded further to incorporate a reaction equation, and eventually simulate 
+% 1-D detonations.
 
 % units: g, mm, s, Pa
 
-% Fluid materials
-% materials = [
-%   VarFcnJWL(0.28, 3.712e11, 3.23e9, 4.15, 0.95, 1.63e-3);
-%   VarFcnSG(7.15, 3.309e8);
-% ];
-
-materials = [
-  VarFcnSG(1.4, 0);
-  VarFcnSG(1.667, 0);
-];
+% Mixture model -- has two fluid materials (i.e. VarFcn's).
+mixture = MixtureModel(VarFcnSG(1.4, 0), VarFcnSG(1.667, 0));
 
 % Initial Conditions
 % User inputs
-rhoIC = [1.0 1.0];
-uIC = [0 0];
-pIC = [500 0.2];
+rhoIC = [1.0 1.0];  % density 
+uIC = [0 0];        % velocity
+pIC = [500 0.2];    % pressure
+lambdaIC = [1 0];   % volume fraction
 
 % Spatial domain
-N = 1000;
+N = 50;
 x_in = 0; x_out = 1;
 dx = 1/N;
 x = (x_in + dx/2):dx:(x_out-dx/2);
@@ -33,69 +32,49 @@ x = (x_in + dx/2):dx:(x_out-dx/2);
 % Temporal parameters
 t      = 0; 
 dt     = 1e-5; 
-tf     = 0.015;
+tf     = 1.5e-2;
 n      = 0;
 nsteps = round(tf/dt);
 
 % Interface location
-% NOTE: `x_int` stores the x co-ordinate of the material interface. This example has only one.
-% However, more material interfaces can be present. In such cases, `x_int` should contain
-% x co-ordinate of all interfaces (i.e. an array).
-x_interface = [0.5];
-num_of_interfaces = length(x_interface);
-
-% Check not needed but added for completeness
-if numel(materials) ~= num_of_interfaces+1
-    if numel(materials) < num_of_interfaces+1
-        error("*** Error: Required fluid materials are %d, whereas only %d were provided.", ...
-            num_of_interfaces+1, numel(materials));
-    elseif numel(materials) > num_of_interfaces+1
-        error("*** Error: Required interface locations are %d, whereas only %d were provided.", ...
-            numel(materials)-1, num_of_interfaces);
-    end 
-end
+% `x_int` stores the x co-ordinate of the material interface.
+x_interface = 0.5;
 
 start_time = tic;
 
 % Setup initial state matrix
-% Only 3 primitive variables; [\rho, u_x, p]
-Vn = zeros(3, N);
+% Only 5 primitive variables; [\rho_1, \rho_2, u_x, p, \lambda]
+Vn = zeros(5, N);
 % Not used. Kept here for reference.
-% Only 3 conservative variables; [\rho, \rho u_x, E]
-% Un = zeros(3, N);
-% Stores material ids
-Idn = zeros(1, N);
-
-AllIds = 1:numel(materials);
+% Only 5 conservative variables; [\rho_1*\lambda, \rho_2*(1-\lambda), \rho u_x, E]
+% Un = zeros(5, N);
 
 % Populate primitive variables and material ids
-Vn(1, :) = rhoIC(end)*ones(1, N);
-Vn(2, :) = uIC(end)*ones(1, N);
-Vn(3, :) = pIC(end)*ones(1, N);
-Idn = AllIds(end)*ones(size(Idn));
-index_bounds = ones(1, num_of_interfaces+1);
-for j = 1:num_of_interfaces    
-    index_bounds(j+1) = find(diff(sign(x - x_interface(j))));
+for i=1:N
+   
+    if x(i) <= x_interface
+%        Left of interface
+        Vn(1, i)   = rhoIC(1);
+        Vn(2, i)   = eps;
+        Vn(3, i)   = uIC(1);
+        Vn(4, i)   = pIC(1);
+        Vn(5, i)   = lambdaIC(1);
+    else
+%        Right of interface
+        Vn(1, i)   = eps;
+        Vn(2, i)   = rhoIC(2);
+        Vn(3, i)   = uIC(2);
+        Vn(4, i)   = pIC(2);
+        Vn(5, i)   = lambdaIC(2);
+    end
     
-    index = index_bounds(j):index_bounds(j+1);
-    
-    Vn(1, index) = rhoIC(j);
-    Vn(2, index) = uIC(j);
-    Vn(3, index) = pIC(j);
-    Idn(index) = AllIds(j);
 end
-
-% Interface x co-ordinate -- might be redundant; could just use Idn
-Xn = x_interface;
 
 % For post-processing
 % indices for accessing state variables from post processing matrices
-keys = reshape(1:3*N, 3, N);
-VVn = zeros(3*N, nsteps+1);
-XXn = zeros(num_of_interfaces, nsteps+1);
-
-VVn(:, 1) = reshape(Vn, [3*N, 1]);
-XXn(:, 1) = reshape(Xn, [num_of_interfaces, 1]);
+keys = reshape(1:5*N, 5, N);
+VVn = zeros(5*N, nsteps+1);
+VVn(:, 1) = reshape(Vn, [5*N, 1]);
 
 % =================================================================================================
 % Time loop
@@ -106,95 +85,12 @@ while t<=tf
     t = t + dt;
     
     fprintf("Step %d: t = %e, dt = %e. Computation time: %.4e s.\n", n, t, dt, toc(start_time));
-    
-    u = Vn(2, :);
-    p = Vn(3, :);
-    
-%     Interface Tracking Routine 
-    Xnp = Xn;
-    Idnp = Idn;
-
-%     Advect the material interface(s)
-    for j = 1:num_of_interfaces
         
-        Xn(j) = real(Xn(j));
-        index = find(diff(sign(x - Xn(j))));
-        
-        x_low  = x(index);
-        x_high = x(index+1);
-        u_low  = u(index);
-        u_high = u(index+1);
-        
-        c1 = (Xn(j) - x_low)/(x_high - x_low);
-        c2 = 1 - c1;
-        
-        u_interface = c1*u_high + c2*u_low;
-        Xnp(j) = Xn(j) + u_interface*dt;
-        
-    end
-    
-%     Update material ids
-    index_bounds = ones(1, num_of_interfaces+1);
-    for j = 1:num_of_interfaces    
-        index_bounds(j+1) = find(diff(sign(x - Xnp(j))));
-
-        index = index_bounds(j):index_bounds(j+1);
-        Idnp(index) = AllIds(j);
-    end
-    
-%     Phase update -- refer Arthur Rallu's thesis.
-    for i=1:N
-       if Idnp(i) ~= Idn(i)
-%           Compute state from neighbors
-           sum_weight    = 0.0;
-           sum_weight_Vn = zeros(3, 1);
-           
-%            This part of the code is taken directly from M2C.
-           for neighi = (i-1):(i+1)
-               if Idnp(neighi) ~= Idnp(i)
-                   continue;
-               end
-               
-               if Idnp(neighi) ~= Idn(neighi)
-%                    this node also changed ID skip. This also skips [i]
-                   continue;
-               end
-               
-%                Upwinding
-               u_neigh = Vn(2, neighi);
-               u_neigh_norm = norm(u_neigh);
-
-               if u_neigh_norm ~= 0
-                   u_neigh = u_neigh/u_neigh_norm;
-               end
-
-               xneighxi = x(i) - x(neighi);
-               xneighxi = xneighxi/norm(xneighxi);
-               weight   = max(0.0, xneighxi*u_neigh);
-
-               if weight > 0
-                   sum_weight = sum_weight + weight;
-                   sum_weight_Vn = weight*Vn(:, neighi);
-               end
-               
-           end
-           
-           if sum_weight == 0
-               error("*** Error: Unable to update phase change at (%d)(%e) by " ...
-                   + "extrapolation w/ upwinding.", i, x(i));
-           end
-           Vn(:, i) = sum_weight_Vn/sum_weight;
-       end
-    end
-    
 %     Advance in time (uses forward euler method and Exact Riemann solver)
-    Vn  = AdvanceOneTimeStep(Vn, materials, x, Idnp, dt);
-    Idn = Idnp;
-    Xn  = Xnp;
+    Vn  = AdvanceOneTimeStep(Vn, mixture, x, dt);
     
 %     Save data and update loop
-    VVn(:, n) = reshape(Vn, [3*N, 1]);
-    XXn(:, n) = reshape(Xn, [num_of_interfaces, 1]);
+    VVn(:, n) = reshape(Vn, [5*N, 1]);
 
 end
 
@@ -212,9 +108,9 @@ grid on;
 box on;
 hold on;
 plot(x, rhot(:, end), 'LineWidth', 2);
-for k = 1:num_of_interfaces
-    xline(XXn(k, end),'--', 'linewidth', 1)
-end
+% for k = 1:num_of_interfaces
+%     xline(XXn(k, end),'--', 'linewidth', 1)
+% end
 hold off;
 title('Density')
 txt = ['t = ' num2str(t_vec(end))];
@@ -226,23 +122,23 @@ grid on;
 box on;
 hold on;
 plot(x, pt(:, end), 'LineWidth', 2);
-for k = 1:num_of_interfaces
-    xline(XXn(k, end),'--', 'linewidth', 1)
-end
+% for k = 1:num_of_interfaces
+%     xline(XXn(k, end),'--', 'linewidth', 1)
+% end
 hold off;
 title('Pressure')
 txt = ['t = ' num2str(t_vec(end))];
 text(0.1,0.1,txt, 'Units', 'normalized');
 
 % Velocity plot
-figure(2)
+figure(3)
 grid on;
 box on;
 hold on;
 plot(x, ut(:, end), 'LineWidth', 2);
-for k = 1:num_of_interfaces
-    xline(XXn(k, end),'--', 'linewidth', 1)
-end
+% for k = 1:num_of_interfaces
+%     xline(XXn(k, end),'--', 'linewidth', 1)
+% end
 hold off;
 title('Velocity')
 txt = ['t = ' num2str(t_vec(end))];
@@ -250,15 +146,16 @@ text(0.1,0.1,txt, 'Units', 'normalized');
 
 % Post processing (animation)
 close all
+figure(4)
 ff = 20; % frames per second
 for n = 1:ff:nsteps
 % % % Toggle comments to plot different variables % % %
 %     plot(x, rhot(:, n), 'linewidth', 2); a = 1;
 %     plot(x, ut(:, n), 'linewidth', 2); a = 2;
     plot(x, pt(:, n), 'linewidth', 2); a = 3;
-    for k = 1:num_of_interfaces
-        xline(XXn(k, n),'--', 'linewidth', 1)
-    end
+%     for k = 1:num_of_interfaces
+%         xline(XXn(k, n),'--', 'linewidth', 1)
+%     end
     txt = ['t = ' num2str(t_vec(n))];
     text(0.1,0.1,txt, 'Units', 'normalized');
     
---
+++ MixtureModel.m
@@ -0,0 +1,213 @@
+% This model represents the mixture equation of state. It takes
+% two different equations of state and the volume fraction parameter
+% which should present material 1 when 1 and material 2 when 0.
+
+% Mixture properties are given as,
+%   1. Density         : \rho = \rho_1*\lambda + \rho_2*(1 - \lambda)
+%   2. Internal energy : \rho*e = \rho_1*e_1*\lambda + \rho_2*e_2*(1 - \lambda)
+%   3. Pressure        : isobaric i.e., p_1 = p_2 = p
+
+classdef MixtureModel
+   
+    properties (SetAccess = private)
+       
+        mat1 VarFcnBase
+        mat2 VarFcnBase
+        
+    end
+    
+    methods (Access = public)
+        function obj = MixtureModel(mat_1, mat_2)
+            arguments
+                mat_1 VarFcnBase
+                mat_2 VarFcnBase
+            end
+            
+            obj.mat1 = mat_1;
+            obj.mat2 = mat_2;
+            
+        end
+%         State conversion methods
+        function U = PrimitiveToConservative(obj, V)
+            % Performing a primitive to conservative operation
+            arguments
+                obj
+%                 mustBeNumeric might be redundant
+                V(5, 1) double {mustBeNumeric, mustBeReal}
+            end
+
+            % Primitive state variables
+            % V[1] -> \rho_1
+            % V[2] -> \rho_2
+            % V[3] -> u_x
+            % V[4] -> p
+            % V[5] -> \lambda
+
+            % Conservative state variables
+            % U[1] -> \rho_1*\lambda
+            % U[2] -> \rho_2*(1 - \lambda)
+            % U[3] -> \rho u_x
+            % U[4] -> E
+            % U[5] -> \lambda
+
+            U = V;
+
+            U(1) = V(5)*V(1);
+            U(2) = (1 - V(5))*V(2);
+            
+            rho  = U(1) + U(2);
+            U(3) = rho*V(3);
+            
+            e = obj.GetInternalEnergyPerUnitMass(V(1), V(2), V(4), V(5)); % pass rho_1, rho_2, p and lambda
+            U(4) = rho*(e + 0.5*V(3)*V(3));
+            
+            U(5) = V(5);
+        end
+        function V = ConservativeToPrimitive(obj, U)
+            % Performing a conservative to primitive operation
+            arguments
+                obj
+%                 mustBeNumeric might be redundant
+                U(5, 1) double {mustBeNumeric, mustBeReal}
+            end
+
+            % Primitive state variables
+            % V[1] -> \rho_1
+            % V[2] -> \rho_2
+            % V[3] -> u_x
+            % V[4] -> p
+            % V[5] -> \lambda
+
+            % Conservative state variables
+            % U[1] -> \rho_1*\lambda
+            % U[2] -> \rho_2*(1 - \lambda)
+            % U[3] -> \rho u_x
+            % U[4] -> E
+            % U[5] -> \lambda
+
+            V = U;
+            
+            rho = U(1) + U(2);
+            invrho = 1/rho;
+        
+%             Avoid division by zero
+            if U(5) < eps
+                V(1) = eps;
+            elseif abs(1 - U(5)) < eps
+                V(2) = eps;
+            else
+                V(1) = U(1)/U(5);
+                V(2) = U(2)/(1 - U(5));
+            end
+ 
+            V(3) = U(3)*invrho;
+            
+            e = (U(4) - 0.5*rho*V(3)*V(3))*invrho;
+            V(4) = obj.GetPressure(V(1), V(2), e, V(5)); % pass rho_1, rho_2, e and lambda
+            
+            V(5) = U(5);
+        end
+        function c = GetSoundSpeed(obj, rho_1, rho_2, e, lambda)
+            arguments
+               obj
+               rho_1 double
+               rho_2 double
+               e double
+               lambda double
+            end
+
+            rho  = lambda*rho_1 + (1 - lambda)*rho_2;
+            p    = obj.GetPressure(rho_1, rho_2, e, lambda);
+            e_1  = obj.mat1.GetInternalEnergyPerUnitMass(rho_1, p);
+            e_2  = obj.mat2.GetInternalEnergyPerUnitMass(rho_2, p);
+            
+            c_1 = obj.mat1.GetSoundSpeed(rho_1, e_1);
+            c_2 = obj.mat2.GetSoundSpeed(rho_2, e_2);
+
+            Gamma1 = obj.mat1.GetBigGamma(rho_1, e_1);
+            Gamma2 = obj.mat2.GetBigGamma(rho_2, e_2);
+            
+%             TODO: Need to check if these relations are valid.
+            Xi1    = 1/Gamma1;
+            Xi2    = 1/Gamma2;
+            Xi     = lambda*Xi1 + (1 - lambda)*Xi2;
+            
+            y1     = lambda*rho_1/rho;
+            y2     = (1 - lambda)*rho_2/rho;
+            
+            c2     = (y1*Xi1*c_1*c_1 + y2*Xi2*c_2*c_2)/Xi;
+            if c2 <= 0
+                error("*** Error: Cannot calculate speed of sound for the mixture " ...
+                    + "(Square-root of a negative number)")
+            end
+            c      = sqrt(c2);
+            
+        end
+        
+        function p = GetPressure(obj, rho_1, rho_2, e, lambda)
+            arguments
+                obj
+                rho_1 double
+                rho_2 double
+                e double
+                lambda double
+            end
+            
+%             We solve the pressure equation iteratively. Solution should exist as for general
+%             equation of state (e.g., Mie_Gr\:uneisen) pressure can be defined explicitly in terms
+%             for fractional densities and internal energy (e). 
+%             NOTE: With iterative approach we can use existing implementations of VarFcn's w/o
+%             adding new functinalities.
+%             This might slow down the code.
+
+            eqn     = @(x) obj.Fun(rho_1, rho_2, e, lambda, x);
+            options = optimoptions('fsolve', 'Display', 'iter', 'MaxIterations', 20);
+            try
+                p       = fsolve(eqn, 0, options);
+            catch ME
+                error("rho_1: %e, rho_2: %e, e: %e, lambda: %e\n", rho_1, rho_2, e, lambda);
+            end
+            
+        end
+        function e = GetInternalEnergyPerUnitMass(obj, rho_1, rho_2, p, lambda)
+            arguments
+                obj
+                rho_1 double
+                rho_2 double
+                p double
+                lambda double
+            end
+            
+            e_1 = obj.mat1.GetInternalEnergyPerUnitMass(rho_1, p);
+            e_2 = obj.mat2.GetInternalEnergyPerUnitMass(rho_2, p);
+            
+            rho    = lambda*rho_1 + (1 - lambda)*rho_2;
+            e      = (rho_1*e_1 + rho_2*e_2)/rho;
+            
+        end
+        
+    end
+    
+    methods (Access = private)
+%        \rho*e = \rho_1*e_1*\lambda + \rho_2*e_2*(1 - \lambda)
+        function r = Fun(obj, rho_1, rho_2, e, lambda, p)
+            arguments
+                obj
+                rho_1 double
+                rho_2 double
+                e double
+                lambda double
+                p double
+            end
+            
+            rho    = lambda*rho_1 + (1 - lambda)*rho_2;
+            
+            term1  = lambda*rho_1*obj.mat1.GetInternalEnergyPerUnitMass(rho_1, p);
+            term2  = (1 - lambda)*rho_2*obj.mat2.GetInternalEnergyPerUnitMass(rho_2, p);
+            
+            r = rho*e - term1 - term2;
+        end
+        
+    end
+    
+end
